259e3fa9798b4cf9b3f6516d4594faa4b81bc959
luisbesil289 <l.besil@gmail.com>
2019-07-03 13:36:36 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import barra from './barra';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+       <barra />
       </header>
     </div>
   );
