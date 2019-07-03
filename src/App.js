0603689d04e969d1990d864d37b0ed1a48edce98
luisbesil289 <l.besil@gmail.com>
2019-07-03 15:14:26 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import barra from './barra';
+import Barra from './Barra';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <barra />
+       <Barra />
       </header>
     </div>
   );
