e491b63eeff3dcc3f4e6be56cc29b52016f2a7b4
luisbesil289 <l.besil@gmail.com>
2019-07-06 19:23:57 -0300

    arrancando

--- src/App.js
+++ src/App.js
@@ -2,10 +2,41 @@ import React from 'react';
 import AppContext from './AppContext';
 import './App.css';
 import MenuPrincipal from './MenuPrincipal';
+import Home from './Home';
+import Race from './Race';
 import Footer from './Footer';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      section: 1
+    });
+  }
+
+  goToHome = () => {
+    this.setState({
+      section: 1
+    });
+  }
+
+  goToNew = () => {
+    this.setState({
+      section: 2
+    });
+  }
+
+  currentSection() {
+    if (this.state.section === 1) {
+      return <Home />;
+    }
+
+    if (this.state.section === 2) {
+      return <Race />;
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,12 +47,13 @@ class App extends React.Component {
           products: [],
           addPotato: [],
           clearProductList: [],
-          addNewProduct: []}}>
+          addNewProduct: []
+        }}>
           <main>
 
           </main>
         </AppContext.Provider>
-
+        {this.currentSection()}
         <Footer />
       </div>
     )
--- src/Footer.js
+++ src/Footer.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './footer.css';
 
+
 class Footer extends React.Component {
     render() {
         return (
-            <div>
+            <div className="footer">
                 <div id="footer_social">Stay tuned, follow us:
             <br />
                     <div>
-                        <a href="https://www.youtube.com/user/nastycop420"><img className="footerIcon" src="assets/youtube.png"></img></a>
-                        <a href="https://www.facebook.com/groups/BlackSheepLOUNGE"><img className="footerIcon" src="assets/facebook.png"/></a>
-                        <a href="https://www.instagram.com/blacksheepfpv/"><img className="footerIcon" src="assets/instagram.png"/></a>
-                        <a href="https://plus.google.com/+nastycop420"><img className="footerIcon" src="assets/twitter.png"/></a>
-                        <a href="https://twitter.com/blacksheepfpv"><img className="footerIcon" src="assets/whatsapp.png"/></a>
+                        <a href="https://www.youtube.com/user/nastycop420"><img className="footerIcon" alt='icon' src='assets/youtube.png'></img></a>
+                        <a href="https://www.facebook.com/groups/BlackSheepLOUNGE"><img className='footerIcon' alt='icon' src={'assets/facebook.png'}/></a>
+                        <a href="https://www.instagram.com/blacksheepfpv/"><img className="footerIcon" alt='icon' src="assets/instagram.png"/></a>
+                        <a href="https://plus.google.com/+nastycop420"><img className="footerIcon" alt='icon' src="assets/twitter.png"/></a>
+                        <a href="https://twitter.com/blacksheepfpv"><img className="footerIcon" alt='icon' src="assets/whatsapp.png"/></a>
                     </div>
                 </div>
                 <p className="copyright"> Copyright© 2019 <a href="mailto:l.besil@gmail.com" > Luis Besil </a> - All Rights Reserved.</p>
