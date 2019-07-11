ea726a9934c849100056f4f575ecf2596cb2c0f4
luisbesil289 <l.besil@gmail.com>
2019-07-11 20:49:27 -0300

    blog

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -1,116 +1,352 @@
 import React from 'react';
-import AppContext from '../AppContext';
-import './Blog.css';
-import BlogCard from './BlogCard';
+/*  import { makeStyles } from '@material-ui/core/styles'; */ 
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Hidden from '@material-ui/core/Hidden';
+import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Container from '@material-ui/core/Container';
+import Markdown from './Markdown';
+import post1 from './blog-post.1.md';
+import post2 from './blog-post.2.md';
+import post3 from './blog-post.3.md';
 
-class BlogsList extends React.Component {
-  static contextType = AppContext;
-  constructor(props) {
-    super(props);
-    this.unBlog = []
-  }
+function MadeWithLove() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Built with love by the '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Material-UI
+      </Link>
+      {' team.'}
+    </Typography>
+  );
+}
 
-  changeTitulo = (event) => {
-    this.unBlog.titulo = event.target.value;
-  }
-  changeFamilia = (event) => {
-    this.unBlog.usuario = event.target.value;
-  }
-  changeFecha = (event) => {
-    this.unBlog.fecha = event.target.value;
-  }
-  changeFoto = (event) => {
-    var str = event.target.value;
-    var res = str.replace(/^C:\\fakepath\\/, "assets/"); //me encontre con que la imagen se queria guardar en c:\fakepath
-    this.unBlog.foto = res;
-  }
-  changeComentario = (event) => {
-    this.unBlog.descripcion = event.target.value;
-  }
+const useStyles = theme => ({
+  toolbar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+  toolbarSecondary: {
+    justifyContent: 'space-between',
+    overflowX: 'auto',
+  },
+  toolbarLink: {
+    padding: theme.spacing(1),
+    flexShrink: 0,
+  },
+  mainFeaturedPost: {
+    position: 'relative',
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+    backgroundImage: 'url(https://source.unsplash.com/user/erondu)',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,.3)',
+  },
+  mainFeaturedPostContent: {
+    position: 'relative',
+    padding: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing(6),
+      paddingRight: 0,
+    },
+  },
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+  card: {
+    display: 'flex',
+  },
+  cardDetails: {
+    flex: 1,
+  },
+  cardMedia: {
+    width: 160,
+  },
+  markdown: {
+    ...theme.typography.body2,
+    padding: theme.spacing(3, 0),
+  },
+  sidebarAboutBox: {
+    padding: theme.spacing(2),
+    backgroundColor: theme.palette.grey[200],
+  },
+  sidebarSection: {
+    marginTop: theme.spacing(3),
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    marginTop: theme.spacing(8),
+    padding: theme.spacing(6, 0),
+  },
+});
 
-  getNextBlogId() {
-    var productWithHighestId = this.context.blog.sort((a, b) => b.id - a.id)[0];
-    if (productWithHighestId === undefined) {
-      return 1; // List is empty, so use 1 as first product id.
-    } else {
-      return productWithHighestId.id + 1; // List is not empty, increase one to the highest id for the next product.
-    }
+const sections = [
+  'Technology',
+  'Design',
+  'Culture',
+  'Business',
+  'Politics',
+  'Opinion',
+  'Science',
+  'Health',
+  'Style',
+  'Travel',
+];
+
+const featuredPosts = [
+  {
+    title: 'Featured post',
+    date: 'Nov 12',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+  },
+];
+
+const posts = [post1, post2, post3];
+
+const archives = [
+  'March 2020',
+  'February 2020',
+  'January 2020',
+  'December 2019',
+  'November 2019',
+  'October 2019',
+  'September 2019',
+  'August 2019',
+  'July 2019',
+  'June 2019',
+  'May 2019',
+  'April 2019',
+];
+
+const social = ['GitHub', 'Twitter', 'Facebook'];
+/* const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  paper: {
+    padding: theme.spacing(3, 2),
+    margin: theme.spacing(2, 0),
+    width: '100%'
+  },
+  textField: {
+    marginRight: theme.spacing(4),
+    width: 300,
+  },
+  textArea: {
+  },
+  dense: {
+    marginTop: 19,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  avatar: {
+    margin: 10,
+  },
+  menu: {
+    width: 200,
   }
-
-  addToBlog = (newBlog) => {
-    this.context.blog.push({
-      id: this.getNextBlogId(), titulo: newBlog.titulo,
-      usuario: newBlog.usuario,
-      fecha: newBlog.fecha,
-      foto: 'assets/DRL_BMW_Welt2018_sw4486.jpg',
-      descripcion: newBlog.descripcion
-    });
-  };
-
-  addToBlogComentarios = (unBlogId) => {
-
-  }
-
-
+});
+ */
+class Blog extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col col-12 col-sm-12 col-md-12">
-          <div className="btn-group">
-              <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#myModal2">Nuevo</button>
-            </div>
-            <hr />
-            {this.context.blog.map(blog => <BlogCard goToBlogComentarios={this.props.goToBlogComentarios} blog={blog} key={blog.id} />)}
-            <hr />
-            <br />
-          </div>         
-        </div>
-
-        {/*  MODAL */}
-        <div className="modal fade" id="myModal2" role="dialog">
-          <div className="modal-dialog" id="ModalFormBlog">
-
-            {/* MODAL CONTENT */}
-            <div className="modal-content">
-              <form id="ModalFormBlog" className="was-validated">
-                <div className="form-group">
-                  <label htmlFor="uname">Titulo</label>
-                  <input type="text" className="form-control" id="titu" onChange={this.changeTitulo} placeholder="Ingrese un Titulo ..." name="titu" required></input>
-                  <div className="valid-feedback">Valid.</div>
-                  <div className="invalid-feedback">Please fill out this field.</div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="text">Familia</label>
-                  <input type="uname" className="form-control" id="fami" onChange={this.changeFamilia} placeholder="Ingrese Nombre/Apellido de su Familia" name="fami" required></input>
-                  <div className="valid-feedback">Valid.</div>
-                  <div className="invalid-feedback">Please fill out this field.</div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="inputFecha">Fecha de Adopcion</label>
-                  <input type="date" className="form-control" id="fech" name="fech" onChange={this.changeFecha} required></input>
-                  <div className="valid-feedback">Valid.</div>
-                  <div className="invalid-feedback">Please fill out this field.</div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="text">Foto</label>
-                  <br />
-                  <br />
-                  <input id="file" type="file" className="form-control" accept="image/png, image/jpeg" onChange={this.changeFoto} />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="text">Comentario</label>
-                  <textarea type="text" className="form-control" placeholder="Comente porque adopto..." onChange={this.changeComentario} required></textarea>
-                </div>
-                <button type="button" className="btn btn-primary btn-block" onClick={(e) => this.addToBlog(this.unBlog, e)}>Guardar</button>
-              </form>
-              <button type="button" className="btn btn-warning btn-block" data-dismiss="modal" onClick={this.limpiarForm}>Close</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="lg">
+          <Toolbar className={this.props.classes.toolbar}>
+            <Button size="small">Subscribe</Button>
+            <Typography
+              component="h2"
+              variant="h5"
+              color="inherit"
+              align="center"
+              noWrap
+              className={this.props.classes.toolbarTitle}
+            >
+              Blog
+          </Typography>
+            <IconButton>
+              <SearchIcon />
+            </IconButton>
+            <Button variant="outlined" size="small">
+              Sign up
+          </Button>
+          </Toolbar>
+          <Toolbar component="nav" variant="dense" className={this.props.classes.toolbarSecondary}>
+            {sections.map(section => (
+              <Link
+                color="inherit"
+                noWrap
+                key={section}
+                variant="body2"
+                href="#"
+                className={this.props.classes.toolbarLink}
+              >
+                {section}
+              </Link>
+            ))}
+          </Toolbar>
+          <main>
+            {/* Main featured post */}
+            <Paper className={this.props.classes.mainFeaturedPost}>
+              {/* Increase the priority of the hero background image */}
+              {
+                <img
+                  style={{ display: 'none' }}
+                  src="https://source.unsplash.com/user/erondu"
+                  alt="background"
+                />
+              }
+              <div className={this.props.classes.overlay} />
+              <Grid container>
+                <Grid item md={6}>
+                  <div className={this.props.classes.mainFeaturedPostContent}>
+                    <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                      Title of a longer featured blog post
+                  </Typography>
+                    <Typography variant="h5" color="inherit" paragraph>
+                      Multiple lines of text that form the lede, informing new readers quickly and
+                      efficiently about what&apos;s most interesting in this post&apos;s contents.
+                  </Typography>
+                    <Link variant="subtitle1" href="#">
+                      Continue reading…
+                  </Link>
+                  </div>
+                </Grid>
+              </Grid>
+            </Paper>
+            {/* End main featured post */}
+            {/* Sub featured posts */}
+            <Grid container spacing={4} className={this.props.classes.cardGrid}>
+              {featuredPosts.map(post => (
+                <Grid item key={post.title} xs={12} md={6}>
+                  <CardActionArea component="a" href="#">
+                    <Card className={this.props.classes.card}>
+                      <div className={this.props.classes.cardDetails}>
+                        <CardContent>
+                          <Typography component="h2" variant="h5">
+                            {post.title}
+                          </Typography>
+                          <Typography variant="subtitle1" color="textSecondary">
+                            {post.date}
+                          </Typography>
+                          <Typography variant="subtitle1" paragraph>
+                            {post.description}
+                          </Typography>
+                          <Typography variant="subtitle1" color="primary">
+                            Continue reading...
+                        </Typography>
+                        </CardContent>
+                      </div>
+                      <Hidden xsDown>
+                        <CardMedia
+                          className={this.props.classes.cardMedia}
+                          image="https://source.unsplash.com/random"
+                          title="Image title"
+                        />
+                      </Hidden>
+                    </Card>
+                  </CardActionArea>
+                </Grid>
+              ))}
+            </Grid>
+            {/* End sub featured posts */}
+            <Grid container spacing={5} className={this.props.classes.mainGrid}>
+              {/* Main content */}
+              <Grid item xs={12} md={8}>
+                <Typography variant="h6" gutterBottom>
+                  From the Firehose
+              </Typography>
+                <Divider />
+                {posts.map(post => (
+                  <Markdown className={this.props.classes.markdown} key={post.substring(0, 40)}>
+                    {post}
+                  </Markdown>
+                ))}
+              </Grid>
+              {/* End main content */}
+              {/* Sidebar */}
+              <Grid item xs={12} md={4}>
+                <Paper elevation={0} className={this.props.classes.sidebarAboutBox}>
+                  <Typography variant="h6" gutterBottom>
+                    About
+                </Typography>
+                  <Typography>
+                    Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit
+                    amet fermentum. Aenean lacinia bibendum nulla sed consectetur.
+                </Typography>
+                </Paper>
+                <Typography variant="h6" gutterBottom className={this.props.classes.sidebarSection}>
+                  Archives
+              </Typography>
+                {archives.map(archive => (
+                  <Link display="block" variant="body1" href="#" key={archive}>
+                    {archive}
+                  </Link>
+                ))}
+                <Typography variant="h6" gutterBottom className={this.props.classes.sidebarSection}>
+                  Social
+              </Typography>
+                {social.map(network => (
+                  <Link display="block" variant="body1" href="#" key={network}>
+                    {network}
+                  </Link>
+                ))}
+              </Grid>
+              {/* End sidebar */}
+            </Grid>
+          </main>
+        </Container>
+        {/* Footer */}
+        <footer className={this.props.classes.footer}>
+          <Container maxWidth="lg">
+            <Typography variant="h6" align="center" gutterBottom>
+              Footer
+          </Typography>
+            <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+              Something here to give the footer a purpose!
+          </Typography>
+            <MadeWithLove />
+          </Container>
+        </footer>
+        {/* End footer */}
+      </React.Fragment >
     );
   }
 }
 
-export default BlogsList;
+export default withStyles(useStyles)(Blog);
